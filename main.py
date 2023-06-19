7547a557126c7e8631c9f9019043d069972fe26c
ninjawill10 <99738445+ninjawill10@users.noreply.github.com>
2023-06-19 08:59:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
 line = 0
+variables = {}
+
 with open("./main.Cb", "r") as file:
   commands = file.readlines()
-  for x in commands:
+  for command in commands:
     line += 1
-    if "Word: " in x:
-      print(x.replace("Word: ", "").strip())
+    tokens = command.strip().split()
+
+    if len(tokens) == 0:
       continue
-    if "InputVar: " in x:
-      varname = x.replace("InputVar: ", "").strip()
-      exec(f"{varname} = input()", globals())
+
+    if tokens[0].lower() == "write":
+      if len(tokens) == 1:
+        print(f"Invalid syntax of '{' '.join(tokens)}' on line {line}")
+      elif len(tokens) > 1:
+        output_values = []
+        for token in tokens[1:]:
+          if token.startswith("$"):
+            var_name = token[1:]
+            if var_name in variables:
+              output_values.append(variables[var_name])
+            else:
+              output_values.append(
+                f"Error: Variable {var_name} is not defined")
+          else:
+            output_values.append(token)
+        print(" ".join(output_values))
+      else:
+        print(f"Syntax error on line {line}")
+    elif len(tokens) > 1 and tokens[1].lower() == "=":
+      if len(tokens) < 3 or tokens[1].lower() != "=":
+        print(f"Invalid syntax of '{' '.join(tokens)}' on line {line}")
+      else:
+        if tokens[2] == "input()":
+          var_name = tokens[0]
+          var_value = input()
+          variables[var_name] = var_value
+        else:
+          var_name = tokens[0]
+          var_value = " ".join(tokens[2:])
+          variables[var_name] = var_value
+    elif tokens[0].lower() == "maths":
+      expression = " ".join(tokens[1:])
+      result = eval(expression)
+      print(result)
+    elif tokens[0].lower() == "#":
       continue
-    if "Var: " in x:
-      Var = x.replace("Var: ", "")
-      try:
-          exec(f"print({Var})")
-      except:
-        print(f"\nError in line {line}:")
-        print(f"The variable requested does not exist please check your spelling")
-      continue
-    if "Word+Var: " in x:
-      print("jeff")
+    elif tokens[0].lower() == "clear":
+      variables = {}
+    else:
+      print(f"Undefined command {tokens[0]} on line {line}")
